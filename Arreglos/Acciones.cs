1c7aa3bea4fecae604107be86368b6ddbb52e3f9
Masecy <Masecy@MASECY>
2025-09-07 21:49:21 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos/Acciones.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    internal class Acciones
+    {
+        private double[] arrnombres;
+       public void Agregar()
+        {
+            Console.WriteLine("Ingrese el numero de elementos del arreglo:");
+            int n = int.Parse(Console.ReadLine());
+            arrnombres = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Ingrese el elemento {i + 1}:");
+                arrnombres[i] = double.Parse(Console.ReadLine());
+            }
+        }
+        public void Mostrar()
+        {
+            foreach (var i in arrnombres)
+            {
+                Console.WriteLine($"Nombres:{i}");
+            }
+        }
+        public double Suma()
+        {
+            return arrnombres.Sum();
+        }
+        public double Promedio()
+        {
+            return arrnombres.Average();
+        }
+        public double Min()
+        {
+            return arrnombres.Min();
+        }
+        public double Max()
+        {
+            return arrnombres.Max();
+        }
+        public double Primero()
+        {
+            return arrnombres.First();
+        }
+        public double Ultimo()
+        {
+            return arrnombres.Last();
+        }
+        public void Ordenar()
+        {
+            Array.Sort(arrnombres);
+        }
+        public void Rev()
+        {
+            Array.Reverse(arrnombres);
+        }
+        public int NumeroElementos()
+        {
+            return arrnombres.Length;
+        }
+
+    }
+}
---
+++ Arreglos/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acciones acciones= new Acciones();
+            while (true)
+            {
+                switch (Menu())
+                {
+                    case 1:
+                        acciones.Agregar();
+                        break;
+                    case 2:
+                        acciones.Mostrar();
+                        break;
+                    case 3:
+                        acciones.Suma();
+                        break;
+                    case 4:
+                        acciones.Promedio();
+                        break;
+                    case 5:
+                        acciones.Min();
+                        break;
+                    case 6:
+                        acciones.Max();
+                        break;
+                    case 7:
+                        acciones.Primero();
+                        break;
+                    case 8:
+                        acciones.Ultimo();
+                        break;
+                    case 9:
+                        acciones.Ordenar();
+                        break;
+                    case 10:
+                        acciones.Rev();
+                        break;
+                    case 11:
+                        acciones.NumeroElementos();
+                        break;
+                    case 12:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
+
+            }
+
+        }
+        static int Menu ()
+        {
+            Console.WriteLine("------------");
+            Console.WriteLine("1)Agregar");
+            Console.WriteLine("2)Mostar");
+            Console.WriteLine("3)Suma");
+            Console.WriteLine("4)Promedio");
+            Console.WriteLine("5)Min");
+            Console.WriteLine("6)Max");
+            Console.WriteLine("7)Primero");
+            Console.WriteLine("8)Ultimo");
+            Console.WriteLine("9)Ordenar");
+            Console.WriteLine("10)Rev");
+            Console.WriteLine("11)Numero de elementos");
+            Console.WriteLine("12)Salir");
+            Console.WriteLine("------------");
+            int opc =Convert.ToInt32( Console.ReadLine());
+            return opc;
+
+        }
+    }
+}
